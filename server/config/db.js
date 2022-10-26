ec0f5a0bfcfe285d64a78842f4ba90ca306d24d6
Awasjo <awasjomail@gmail.com>
2022-10-25 21:43:49 -0400

    Added Atlas Mongodb

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //changed this to atlas
+  "URI": "mongodb+srv://Awas:EAY3v3k7ZV1kGU6J@test.cvlzase.mongodb.net/books229?retryWrites=true&w=majority"
+};
